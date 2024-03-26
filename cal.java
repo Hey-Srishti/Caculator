8a0a233483e2576d87c08d31964b2b5e487b537f
Hey-Srishti <105637555+Hey-Srishti@users.noreply.github.com>
2024-03-26 17:40:17 +0530

    Update cal.js

--- cal.java
+++ cal.java
@@ -1,44 +1,20 @@
-let btns = document.querySelectorAll(".num-button");
-let allBtns = document.querySelectorAll(".button");
-let resultBox = document.querySelector("#result-box");
-let clearBtn = document.querySelector('#clear');
-
-let total = document.querySelector("#total");
-
-let btnSpread = [...btns];
-let allBtnSpread = [...allBtns];
-
-// For Number Inputs
-btnSpread.forEach((button, i) => {
-  button.addEventListener("click", () => {
-    // Inner Values for calculator
-
-    if (resultBox.innerHTML == "0") {
-      resultBox.innerHTML = "";
+// Todo: Make M+ M- and MC functional
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
     }
-
-    let value = btns[i].innerHTML;
-    resultBox.innerHTML += value;
-  });
-});
-
-
-// Function to evalute Strings
-function evaluate(fn) {
-    return new Function('return ' + fn)();
-}
-
-// To calculate All Input
-total.addEventListener('click', ()=> {
-let allInputs = resultBox.innerHTML;
-
-resultBox.innerHTML = evaluate(allInputs);
-
-console.log(evaluate(allInputs));
+    else if(e.target.innerHTML == 'C'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
 })
-
-// Clear all Inputs
-clearBtn.addEventListener('click', ()=> {
-    resultBox.innerHTML = "0";
-})
- 
